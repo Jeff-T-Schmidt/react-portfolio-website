9a57ab2ebdec5ed7c0c3f5b1925e386c2b9530ef
Jeff Schmidt <jeff.t.schmidt@gmail.com>
2022-08-02 11:32:44 -0700

    fixing the home page, colors and spacing

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,15 +2,17 @@ import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
-import LogoTitle from '../../assets/images/logo-s.png'
+import LogoTitle from '../../assets/images/Jeff_S_OG_Black_2022-01.png'
 import Logo from './Logo'
 import './index.scss'
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
 
-  const nameArray = ['l', 'o', 'b', 'o', 'd', 'a', 'n']
+  const nameArray = ['c', 'h', 'm', 'i', 'd', 't']
   const jobArray = [
+    'a',
+    ' ',
     'w',
     'e',
     'b',
@@ -39,10 +41,16 @@ const Home = () => {
         <div className="text-zone">
           <h1>
             <span className={letterClass}>H</span>
-            <span className={`${letterClass} _12`}>i,</span>
+            <span className={`${letterClass} _12`}>i</span>
+            <span className={`${letterClass} _12`}>,</span>
+            <span style={{marginLeft:'15px'}} className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'</span>
+            <span className={`${letterClass} _14`}>m</span>
             <br />
-            <span className={`${letterClass} _13`}>I</span>
-            <span className={`${letterClass} _14`}>'m</span>
+            <span className={`${letterClass} _14`}>J</span>
+            <span className={`${letterClass} _14`}>e</span>
+            <span className={`${letterClass} _14`}>f</span>
+            <span className={`${letterClass} _14`}>f</span>
             <img
               src={LogoTitle}
               alt="JavaScript Developer Name, Web Developer Name"
@@ -59,7 +67,7 @@ const Home = () => {
               idx={22}
             />
           </h1>
-          <h2>Front End Developer / JavaScript Expert / Youtuber</h2>
+          <h2>Full Stack Developer</h2>
           <Link to="/contact" className="flat-button">
             CONTACT ME
           </Link>
