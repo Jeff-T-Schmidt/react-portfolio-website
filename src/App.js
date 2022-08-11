e57a2851dd391a1553bb05f3f1feed2510eff8bd
Jeff Schmidt <jeff.t.schmidt@gmail.com>
2022-08-11 14:39:03 -0700

    Added GA to React components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import { Route, Routes } from 'react-router-dom'
+import React, {useEffect } from 'react';
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
 import Layout from './components/Layout'
 import Project from './components/Project'
 import './App.scss'
+import ReactGA from 'react-ga';
+const TRACKING_ID = "G-PNHVXJRZN8";
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <>
       <Routes>
