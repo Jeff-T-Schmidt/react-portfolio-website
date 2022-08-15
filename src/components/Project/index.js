5b15584a61bf74ba8eab1b1bef29cc7c3046f0f0
Jeff Schmidt <jeff.t.schmidt@gmail.com>
2022-08-15 13:55:08 -0700

    Fixed some spacing of the project images and links

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -32,7 +32,7 @@ const Project = () => {
                         <a href='https://jeff-t-schmidt.github.io/Web-API-Quiz/' target="_blank" rel="noopener noreferrer"><img src={image1} className="projectImages" /></a>
                         <a href="https://jeff-t-schmidt.github.io/passwordGen/" target="_blank" rel="noopener noreferrer"> <img src={image2} className="projectImages" /></a>
                         <a href="https://hidden-shore-04969.herokuapp.com/home" target="_blank" rel="noopener noreferrer" > <img src={image3} className="projectImages" /></a>
-                        <a href="https://jeff-t-schmidt.github.io/First-Project/" target="_blank" rel="noopener noreferrer"> <img src={image4} className="projectImages" /></a>
+                        <a href="https://jeff-t-schmidt.github.io/First-Project/" target="_blank" rel="noopener noreferrer"> <img src={image4} className="projectImages" marginRight={'20px'}/></a>
                     </div>
                     <div className='titles'>
                         <a href='https://github.com/Jeff-T-Schmidt/Web-API-Quiz' className='title1' target="_blank" rel="noopener noreferrer">Code Quiz</a>
